33b29ccb2a3f50f5bae8a00edb9754fc64b07685
dvaidlandy <landydavidjohn@gmail.com>
2022-08-09 16:27:22 +0100

    Phones storage

---
+++ app/src/main/java/com/example/quartermaster/Phone.java
@@ -0,0 +1,15 @@
+package com.example.quartermaster;
+
+import java.util.PrimitiveIterator;
+
+public class Phone {
+    private String storage,model,modelNumber,colour,brand,nameOwner;
+    public Phone(String model,String modelNumber,String nameOwner,String colour,String brand){
+        this.brand=brand;
+        this.model=model;
+        this.colour=colour;
+        this.modelNumber=modelNumber;
+        this.nameOwner=nameOwner;
+    }
+
+}
