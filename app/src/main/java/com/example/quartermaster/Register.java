7407eeac9f65f55b50c0d1c061d1fceb5e1f52fb
DAericsson <110463884+DAericsson@users.noreply.github.com>
2022-08-09 13:56:49 +0100

    Begining to use code from video

--- app/src/main/java/com/example/quartermaster/Register.java
+++ app/src/main/java/com/example/quartermaster/Register.java
@@ -1,23 +1,22 @@
 package com.example.quartermaster;
 
 import static android.content.ContentValues.TAG;
-
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
-
+import com.google.firebase.auth.FirebaseUser;
 
 public class Register extends AppCompatActivity {
-    EditText email, password;
-    Button registerBtn;
+    EditText mFullName, mEmail, mPassword;
+    Button mRegisterBtn, mLoginBtn;
     FirebaseAuth mAuth;
 
 
@@ -27,10 +26,48 @@ public class Register extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         mAuth = FirebaseAuth.getInstance();
+        mFullName = findViewById(R.id.FullName);
+        mEmail = findViewById(R.id.Email);
+        mPassword  = findViewById(R.id.Password);
+        mRegisterBtn = findViewById(R.id.RegisterBtn);
+        mLoginBtn = findViewById(R.id.LoginBtn);
 
+        mRegisterBtn.setOnClickListener(v -> {
+            final String fullName = mFullName.getText().toString().trim();
+            final String email = mEmail.getText().toString().trim();
+            String password = mPassword.getText().toString().trim();
 
+            if(TextUtils.isEmpty(email)){
+                email.setError("Email is Required.");
+                return;
+            }
+
+            if(TextUtils.isEmpty(password)){
+                password.setError("Password is Required.");
+                return;
+            }
+
+            if(password.length() < 6){
+                password.setError("Password Must be >= 6 Characters");
+                return;
+            }
+
+            // register the user in firebase
+
+            mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(task -> {
+                if(task.isSuccessful()){
+
+                    // send verification link
+                    FirebaseUser fuser = mAuth.getCurrentUser();
+                    assert fuser != null;
+                    fuser.sendEmailVerification().addOnSuccessListener(aVoid -> Toast.makeText(Register.this, "Verification Email Has been Sent.", Toast.LENGTH_SHORT).show()).addOnFailureListener(e -> Log.d(TAG, "onFailure: Email not sent " + e.getMessage()));
+                }
+            });
+        });
     }
-
-
-
 }
+
+
+
+
+
