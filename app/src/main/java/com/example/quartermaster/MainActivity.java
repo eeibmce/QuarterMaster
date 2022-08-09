b3aee8bb7511625e54c8434deff757185630613e
Jchen0086 <shengxin.chen@ericsson.com>
2022-08-09 14:13:21 +0100

    Prototype Login and Register btn in main page

--- app/src/main/java/com/example/quartermaster/MainActivity.java
+++ app/src/main/java/com/example/quartermaster/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.quartermaster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void launchLogin(View v){
+        //launch a new activity
+
+        Intent i = new Intent(this, Login.class);
+        startActivity(i);
+
+    }
+
+    public void launchRegister(View v){
+        //launch a new activity
+
+        Intent i = new Intent(this, Register.class);
+        startActivity(i);
+
     }
 }
