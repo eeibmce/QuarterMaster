477a9b23e3cafb0dd1444d8bc62c68990b5a9db8
DAericsson <110463884+DAericsson@users.noreply.github.com>
2022-08-10 11:02:09 +0100

    Load main page and check if logged in.

--- app/src/main/java/com/example/quartermaster/MainActivity.java
+++ app/src/main/java/com/example/quartermaster/MainActivity.java
@@ -2,10 +2,13 @@ package com.example.quartermaster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
     Button mLogOutBtn;
@@ -14,12 +17,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            Toast.makeText(MainActivity.this, "You are logged in", Toast.LENGTH_SHORT).show();
+        } else {
+            Intent i = new Intent(getApplicationContext(), Login.class);
+            startActivity(i);
+        }
+
         mLogOutBtn = findViewById(R.id.LogoutBtn);
         mLogOutBtn.setOnClickListener(v -> {
             FirebaseAuth.getInstance().signOut();//logout
             finish();
         });
+
     }
-
-
 }
