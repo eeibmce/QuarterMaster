40df61ff3fb616752152526115770f5c2b9a5b87
ekocjos <jose.kochumuttam@ericsson.com>
2022-08-15 16:36:30 +0100

    ui design fix

--- app/src/main/java/com/example/quartermaster/MainActivity.java
+++ app/src/main/java/com/example/quartermaster/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.quartermaster;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
+import android.view.View;
 import android.widget.Button;
-import android.widget.Spinner;
 import android.widget.Toast;
+
+//import com.google.zxing.integration.android.IntentIntegrator;
+//import com.google.zxing.integration.android.IntentResult;
+//
+//import android.content.Intent;
+//import android.os.Bundle;
+//import android.widget.ArrayAdapter;
+//import android.widget.Button;
+//import android.widget.Spinner;
+//import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
