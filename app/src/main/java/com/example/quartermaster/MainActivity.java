205fdc06296202a6335be840de4660726a8d672a
lin88225 <110463832+lin88225@users.noreply.github.com>
2022-08-15 14:19:58 +0100

    qr code scanner
    qr code scanner

--- app/src/main/java/com/example/quartermaster/MainActivity.java
+++ app/src/main/java/com/example/quartermaster/MainActivity.java
@@ -1,16 +1,70 @@
 package com.example.quartermaster;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btScan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btScan = findViewById(R.id.bt_scan);
+        btScan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                IntentIntegrator intentIntegrator = new IntentIntegrator(
+                        MainActivity.this
+                );
+                intentIntegrator.setPrompt("For Flash Use Volume Up Key");
+                intentIntegrator.setBeepEnabled(true);
+                intentIntegrator.setOrientationLocked(true);
+                intentIntegrator.setCaptureActivity(Capture.class);
+                intentIntegrator.initiateScan();
+
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode,
+                                    int resultCode,
+                                    @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        IntentResult intentResult = IntentIntegrator.parseActivityResult(
+                requestCode, resultCode, data
+        );
+        if(intentResult.getContents()!=null)
+        {
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+
+            builder.setTitle("Result");
+            builder.setMessage(intentResult.getContents());
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    dialogInterface.dismiss();
+                }
+            });
+            builder.show();
+        }else{
+            Toast.makeText(getApplicationContext(),
+                    "OOPS... You did not scan anything",
+                    Toast.LENGTH_SHORT).show();
+        }
     }
 }
-//Testhuihjigjklgjhkgjhk
-// testing commit location
