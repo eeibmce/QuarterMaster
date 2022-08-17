c5b85398edbbbd30049762867bc15eec3379f9a9
lin88225 <110463832+lin88225@users.noreply.github.com>
2022-08-17 12:02:39 +0100

    build change

--- app/src/main/java/com/example/quartermaster/MainActivity.java
+++ app/src/main/java/com/example/quartermaster/MainActivity.java
@@ -2,15 +2,52 @@ package com.example.quartermaster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.google.zxing.*;
+import com.google.zxing.common.BitMatrix;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
 
 public class MainActivity extends AppCompatActivity {
+    EditText etInput;
+    Button btGenerate;
+    ImageView ivOutput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etInput = findViewById(R.id.et_input);
+        btGenerate = findViewById(R.id.bt_generate);
+        ivOutput = findViewById(R.id.iv_output);
+        btGenerate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String sText = etInput.getText().toString().trim();
+                MultiFormatWriter writer = new MultiFormatWriter();
+
+                try {
+                    BitMatrix matrix = writer.encode(sText, BarcodeFormat.QR_CODE, 350, 350);
+                    BarcodeEncoder encoder = new BarcodeEncoder();
+                    Bitmap bitmap = encoder.createBitmap(matrix);
+                    ivOutput.setImageBitmap(bitmap);
+                    InputMethodManager manager = (InputMethodManager) getSystemService(
+                            Context.INPUT_METHOD_SERVICE
+                    );
+                    manager.hideSoftInputFromWindow(etInput.getApplicationWindowToken(), 0);
+                } catch (WriterException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
-//Testhuihjigjklgjhkgjhk
-// testing commit location
