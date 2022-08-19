faf464a565108b626b7275b5e193fbe510b48d22
Jchen0086 <shengxin.chen@ericsson.com>
2022-08-19 14:30:35 +0100

    Added Search feature

--- app/src/main/java/com/example/quartermaster/ItemListView.java
+++ app/src/main/java/com/example/quartermaster/ItemListView.java
@@ -4,7 +4,10 @@ package com.example.quartermaster;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.Html;
+import android.text.TextUtils;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,6 +19,7 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 
 import java.util.ArrayList;
+import java.util.Locale;
 import java.util.Map;
 
 public class ItemListView extends AppCompatActivity {
@@ -23,8 +27,9 @@ public class ItemListView extends AppCompatActivity {
 
     FirebaseAuth fAuth;
     FirebaseFirestore fStore;
-    Button mItemViewBtn;
+    Button mItemViewBtn, mFilterBtn;
     TextView mUID, mEnterId;
+    Spinner itemViewSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,45 +40,65 @@ public class ItemListView extends AppCompatActivity {
         fStore = FirebaseFirestore.getInstance();
         mUID = findViewById(R.id.UID);
         mEnterId = findViewById(R.id.ItemIdSearch);
+        itemViewSpinner = findViewById(R.id.FilterItems);
+        mFilterBtn = findViewById(R.id.filterBtn);
 
 
-        mUID.setText(Html.fromHtml("<b>Item ID's:</b>"));
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<>(ItemListView.this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.ListofItems));
+        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        itemViewSpinner.setAdapter(myAdapter);
 
-        fStore.collection("Items").get().addOnCompleteListener(task -> {
-            if (task.isSuccessful()) {
-                ArrayList<QueryDocumentSnapshot> docList = new ArrayList<>();
-                for (QueryDocumentSnapshot document : task.getResult()) {
-                    docList.add(document);
+        mFilterBtn.setOnClickListener(v -> {
+                    String itemType = itemViewSpinner.getSelectedItem().toString();
+                    // Checks that item is elected
+                    if (TextUtils.isEmpty(itemType)) {
+                        Toast.makeText(ItemListView.this, "Must Select ItemType", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                    mUID.setText(Html.fromHtml("<b>Item ID's:</b>"));
 
-//                    mUID.append(document.getId());
+                    fStore.collection("Items").get().addOnCompleteListener(task -> {
+                        if (task.isSuccessful()) {
+                            ArrayList<QueryDocumentSnapshot> docList = new ArrayList<>();
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                docList.add(document);
 
-                    fStore.collection("Items").document(document.getId()).get().addOnCompleteListener(tasks -> {
-                        if (tasks.isSuccessful()) {
-                            DocumentSnapshot document2 = tasks.getResult();
-                            if (document2.exists()) {
+                                fStore.collection("Items").document(document.getId()).get().addOnCompleteListener(tasks -> {
+                                    if (tasks.isSuccessful()) {
+                                        DocumentSnapshot document2 = tasks.getResult();
+                                        if (document2.exists()) {
 
-                                mUID.append("\n");
-                                mUID.append("\n -> ");
-                                mUID.append(document.getId());
-                                Map<String, Object> map = document2.getData();
-                                assert map != null;
-                                for (Map.Entry<String, Object> entry : map.entrySet()) {
-                                    mUID.append("\n");
-                                    mUID.append(entry.getValue().toString());
+                                            Map<String, Object> map = document2.getData();
+                                            assert map != null;
+                                            for (Map.Entry<String, Object> entry : map.entrySet()) {
+                                                if (entry.getValue().equals(itemType)) {
+                                                    mUID.append("\n");
+                                                    mUID.append("\n -> ");
+                                                    mUID.append(document.getId());
+
+                                                    mUID.append("\n");
 
 
-                                }
+                                                    mUID.append(map.get("ItemType").toString().toUpperCase(Locale.ROOT));
+                                                    mUID.append("\n");
+                                                    mUID.append(map.get("OwnerEmail").toString().toUpperCase(Locale.ROOT));
+
+
+                                                }
+
+                                            }
+                                        }
+                                    } else {
+                                        Toast.makeText(ItemListView.this, "Does not exist, please check ID and try again", Toast.LENGTH_SHORT).show();
+
+                                    }
+                                });
                             }
-                        } else {
-                            Toast.makeText(ItemListView.this, "Does not exist, please check ID and try again", Toast.LENGTH_SHORT).show();
-
                         }
                     });
-                }
-            }
-        });
 
-
+                });
         mItemViewBtn.setOnClickListener(v -> {
             String Uid = mEnterId.getText().toString();
             Intent i = new Intent(getApplicationContext(), ItemView.class);
