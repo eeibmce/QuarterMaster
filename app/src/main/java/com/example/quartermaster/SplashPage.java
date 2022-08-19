f660bb8b857e9224410ad81441d4f43f80afef84
Jchen0086 <shengxin.chen@ericsson.com>
2022-08-19 16:11:13 +0100

    Fixed Splash Page

--- app/src/main/java/com/example/quartermaster/SplashPage.java
+++ app/src/main/java/com/example/quartermaster/SplashPage.java
@@ -1,36 +1,55 @@
 package com.example.quartermaster;
-
 import android.content.Intent;
 import android.os.Bundle;
-
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashPage extends AppCompatActivity {
+
+    //    Variables
+//    Animation topAnim, bottomAnim;
+//    ImageView imageView3;
+//    TextView textView, textView2;
+    private static final int SPLASH_SCREEN_TIMEOUT = 2000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        startActivity(new Intent(SplashPage.this, HomePage.class));
-        finish();
+        setContentView(R.layout.activity_splash_page);
+//        //Animations
+//        topAnim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+//        bottomAnim = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+//
+//        //Hooks
+//        imageView3 = findViewById(R.id.imageView3);
+//        textView = findViewById(R.id.textView);
+//        textView2 = findViewById(R.id.textView2);
+//
+//        imageView3.setAnimation(topAnim);
+//        textView.setAnimation(bottomAnim);
+//        textView2.setAnimation(bottomAnim);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash_page);
+        Animation fadeOut = new AlphaAnimation(1, 0);
+        fadeOut.setInterpolator(new AccelerateInterpolator());
+        fadeOut.setStartOffset(500);
+        fadeOut.setDuration(1800);
+        ImageView image = findViewById(R.id.imageView8);
+        image.setAnimation(fadeOut);
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashPage.this, HomePage.class);
+                startActivity(intent);
+                finish();
+            }
+
+        }, SPLASH_SCREEN_TIMEOUT);
     }
 }
-
-
-//    private static int SPLASH_SCREEN_TIMEOUT = 2000;
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//
-//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-//                WindowManager.LayoutParams.FLAG_FULLSCREEN);
-//
-//        setContentView(R.layout.activity_splash_page);
-//
-//        Animation fadeOut = new AlphaAnimation(1, 0);
-//        fadeOut.setInterpolator(new AccelerateInterpolator());
-//        fadeOut.setStartOffset(500);
-//        fadeOut.setDuration(1800);
-//        ImageView image = findViewById(R.id.imageView8);
-//        ImageView image1 = findViewById(R.id.imageView10);
-//
-//        image.setAnimation(fadeOut);
