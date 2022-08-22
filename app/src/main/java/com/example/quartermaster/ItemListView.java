b84009c13c3d5f9fe58f5ad6327fa5671432f10c
Jchen0086 <shengxin.chen@ericsson.com>
2022-08-22 16:22:40 +0100

    Fixed Item List View nd Item Create Page

--- app/src/main/java/com/example/quartermaster/ItemListView.java
+++ app/src/main/java/com/example/quartermaster/ItemListView.java
@@ -4,6 +4,7 @@ package com.example.quartermaster;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.text.method.ScrollingMovementMethod;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
@@ -20,12 +21,14 @@ public class ItemListView extends AppCompatActivity {
 
     FirebaseAuth fAuth;
     FirebaseFirestore fStore;
-    Button mItemViewBtn, mFilterBtn;
+    Button mItemViewBtn, mFilterBtn, mItemCreate;
     TextView mUID, mEnterId; //mSearchBar;
     Spinner mSearchBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item_list_view);
         mItemViewBtn = findViewById(R.id.searchBtn);
@@ -36,6 +39,9 @@ public class ItemListView extends AppCompatActivity {
         mEnterId = findViewById(R.id.ItemIdSearch);
         mSearchBar = findViewById(R.id.SearchBar);
         mFilterBtn = findViewById(R.id.filterBtn);
+        mItemCreate = findViewById(R.id.ItemCreate);
+        mUID.setMovementMethod(new ScrollingMovementMethod());
+
 
         ArrayAdapter<String> myAdapter = new ArrayAdapter<>(ItemListView.this,
                 android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.ListofItems));
@@ -80,6 +86,11 @@ public class ItemListView extends AppCompatActivity {
             }
             Intent i = new Intent(getApplicationContext(), ItemView.class);
             i.putExtra("Uid", Uid);
+            startActivity(i);
+        });
+        mItemCreate.setOnClickListener(view -> {
+            finish();
+            Intent i = new Intent(getApplicationContext(), ItemCreate.class);
             startActivity(i);
         });
     }
