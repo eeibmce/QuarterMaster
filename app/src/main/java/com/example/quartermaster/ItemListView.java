b90c2b554e0bdd3e956df1cecef8ff22bb4a6868
eelldar <darragh.elliott@ericsson.com>
2022-08-22 13:10:58 +0100

    Replaced old item list method with new firebase query based one. More efficient

--- app/src/main/java/com/example/quartermaster/ItemListView.java
+++ app/src/main/java/com/example/quartermaster/ItemListView.java
@@ -4,24 +4,15 @@ package com.example.quartermaster;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
-import android.text.Html;
 import android.text.TextUtils;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.Spinner;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
-
-import java.util.ArrayList;
-import java.util.Locale;
-import java.util.Map;
 
 public class ItemListView extends AppCompatActivity {
 
@@ -29,8 +20,7 @@ public class ItemListView extends AppCompatActivity {
     FirebaseAuth fAuth;
     FirebaseFirestore fStore;
     Button mItemViewBtn, mFilterBtn;
-    TextView mUID, mEnterId;
-    Spinner itemViewSpinner;
+    TextView mUID, mEnterId, mSearchBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,76 +29,54 @@ public class ItemListView extends AppCompatActivity {
         mItemViewBtn = findViewById(R.id.searchBtn);
         fAuth = FirebaseAuth.getInstance();
         fStore = FirebaseFirestore.getInstance();
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
         mUID = findViewById(R.id.UID);
         mEnterId = findViewById(R.id.ItemIdSearch);
-        itemViewSpinner = findViewById(R.id.FilterItems);
+        mSearchBar = findViewById(R.id.SearchBar);
         mFilterBtn = findViewById(R.id.filterBtn);
 
 
-        ArrayAdapter<String> myAdapter = new ArrayAdapter<>(ItemListView.this,
-                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.ListofItems));
-        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        itemViewSpinner.setAdapter(myAdapter);
-
         mFilterBtn.setOnClickListener(v -> {
-            String itemType = itemViewSpinner.getSelectedItem().toString();
-            // Checks that item is elected
-            if (TextUtils.isEmpty(itemType)) {
-                Toast.makeText(ItemListView.this, "Must Select ItemType", Toast.LENGTH_SHORT).show();
-                return;
-            }
+            String search = mSearchBar.getText().toString().trim();
             mUID.setTypeface(mUID.getTypeface(), Typeface.BOLD);
             mUID.setText("Item ID's");
             mUID.setTypeface(mUID.getTypeface(), Typeface.NORMAL);
 
-            fStore.collection("Items").get().addOnCompleteListener(task -> {
-                if (task.isSuccessful()) {
-                    ArrayList<QueryDocumentSnapshot> docList = new ArrayList<>();
-                    for (QueryDocumentSnapshot document : task.getResult()) {
-                        docList.add(document);
 
-                        fStore.collection("Items").document(document.getId()).get().addOnCompleteListener(tasks -> {
-                            if (tasks.isSuccessful()) {
-                                DocumentSnapshot document2 = tasks.getResult();
-                                if (document2.exists()) {
-
-                                    Map<String, Object> map = document2.getData();
-                                    assert map != null;
-                                    for (Map.Entry<String, Object> entry : map.entrySet()) {
-                                        if (entry.getValue().equals(itemType)) {
-                                            mUID.append("\n");
-                                            mUID.append("\n -> ");
-                                            mUID.append(document.getId());
-
-                                            mUID.append("\n");
-
-
-                                            mUID.append(map.get("ItemType").toString().toUpperCase(Locale.ROOT));
-                                            mUID.append("\n");
-                                            mUID.append(map.get("OwnerEmail").toString().toUpperCase(Locale.ROOT));
-
-
-                                        }
-
-                                    }
-                                }
-                            } else {
-                                Toast.makeText(ItemListView.this, "Does not exist, please check ID and try again", Toast.LENGTH_SHORT).show();
-
+            db.collection("Items")
+                    .whereEqualTo("ItemType", search)
+                    .get()
+                    .addOnCompleteListener(task -> {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                System.out.println(document.getId() + " => " + document.getData());
+                                mUID.append("\n");
+                                mUID.append(document.getId());
+                                mUID.append("\n");
+                                mUID.append(document.getString("ItemType"));
+                                mUID.append("\n");
+                                mUID.append(document.getString("OwnerEmail"));
+                                mUID.append("\n");
+                                mUID.append(document.getString("ExtraInfo"));
+                                mUID.append("\n");
                             }
-                        });
-                    }
-                }
-            });
+                        } else {
+                            System.out.println("Error getting documents: " + task.getException());
+                        }
+                    });
 
         });
+
+
         mItemViewBtn.setOnClickListener(v -> {
             String Uid = mEnterId.getText().toString();
+            if (TextUtils.isEmpty(Uid)) {
+                mEnterId.setError("Id is Required");
+                return;
+            }
             Intent i = new Intent(getApplicationContext(), ItemView.class);
             i.putExtra("Uid", Uid);
             startActivity(i);
         });
     }
-
-
 }
