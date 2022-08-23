4115b5bc18acfb56db034569a689919e7307ec1a
Jchen0086 <shengxin.chen@ericsson.com>
2022-08-23 10:23:29 +0100

    Fixed design on item list view page

--- app/src/main/java/com/example/quartermaster/ItemListView.java
+++ app/src/main/java/com/example/quartermaster/ItemListView.java
@@ -1,14 +1,18 @@
 package com.example.quartermaster;
 
 
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.text.method.ScrollingMovementMethod;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -41,6 +45,7 @@ public class ItemListView extends AppCompatActivity {
         mFilterBtn = findViewById(R.id.filterBtn);
         mItemCreate = findViewById(R.id.ItemCreate);
         mUID.setMovementMethod(new ScrollingMovementMethod());
+        mUID. setTextIsSelectable(true);
 
 
         ArrayAdapter<String> myAdapter = new ArrayAdapter<>(ItemListView.this,
@@ -92,6 +97,15 @@ public class ItemListView extends AppCompatActivity {
             finish();
             Intent i = new Intent(getApplicationContext(), ItemCreate.class);
             startActivity(i);
+        });
+
+        mUID.setOnClickListener(new View.OnClickListener() { // set onclick listener to my textview
+            @Override
+            public void onClick(View view) {
+                ClipboardManager cm = (ClipboardManager)getApplicationContext().getSystemService(Context.CLIPBOARD_SERVICE);
+                cm.setText(mUID.getText().toString());
+                Toast.makeText(getApplicationContext(), "Copied :)", Toast.LENGTH_SHORT).show();
+            }
         });
     }
 }
